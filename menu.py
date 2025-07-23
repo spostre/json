a065c01e89c73edfec15535ccd54b4443dbda836
spostre <217265511+spostre@users.noreply.github.com>
2025-07-23 16:29:21 -0500

    feat: :rocket: aprendizaje json taller
    codigo incompleto de como usar los json

---
+++ menu.py
@@ -0,0 +1,78 @@
+import json
+import os
+
+def menu():
+    print('---Menu de contactos---')
+    print('1. Listar todos los contactos')
+    print('2. Crear nuevo contacto')
+    print('3. Buscar contacto por ID')
+    print('4. Actualizar contacto')
+    print('5. Eliminar contacto')
+
+    try:
+        opcion = int(input(' : '))
+        if (opcion == 1):
+            leer_contacto()
+            input('Presione Enter para continuar...')
+            return menu()
+        
+        elif (opcion == 2):
+            crear_contacto()
+            input('Presione Enter para continuar...')
+            return menu()
+        
+        elif (opcion == 3):
+            pass
+        elif (opcion == 4):
+            pass
+        elif (opcion == 5):
+            pass
+        
+    except ValueError:
+        print('valor no valido')
+        input('Enter para continuar...')
+
+def leer_contacto():
+       
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    try:
+        with open('telefono.json', "r") as file:
+            contactos = json.load(file)
+            print(contactos)
+
+    except FileNotFoundError as fn:
+        print('dato no encontrado')
+        return menu()
+    except Exception as e:
+        print('dato no encontrado')
+        return menu()
+    
+def crear_contacto(fileName: str) -> bool:
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    try:
+        with open('telefono.json', "w") as file:
+            json.dump('telefono.json', file, indent=4)
+            return True
+    except Exception as e:
+        return False
+
+    except Exception:
+        print('Error desconocido...')
+        return menu()
+
+def busca_contacto():
+
+    try:
+        with open('telefono.json', "r") as file:
+        search = json.load(file)
+        print(search)
+
+
+
+    
+
+if __name__ == "__main__":
+    menu()
